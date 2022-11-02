987f1ccacf108bc3fc1d883202765c056ae836a6
Alexandre Chau <accounts@chau.moe>
2022-11-02 15:29:56 +0100

    test: refactor mock api

--- test/service-api.test.ts
+++ test/service-api.test.ts
@@ -8,86 +8,53 @@ import { BuildAppType, buildApp } from './app';
 import { TEST_ENV } from './config';
 import { MOCK_ITEM, mockTask } from './fixtures';
 
+const MOCK_GROUP_ID = 'g.s8oes9dhwrvt0zif';
+
+type EtherpadApiResponse<T> = [
+  statusCode: number,
+  payload?: { code: number; message: string; data: T },
+];
+
+type Api = {
+  createGroupIfNotExistsFor?: EtherpadApiResponse<{ groupID: string }>;
+  createGroupPad?: EtherpadApiResponse<null>;
+  deletePad?: EtherpadApiResponse<null>;
+};
+
+/**
+ * Helper to setup an emulator for the etherpad server
+ * @param replies Enables which endpoints should be emulated with the given responses
+ */
+function setUpApi(replies: Api): Promise<{ [Endpoint in keyof Api]: URLSearchParams }> {
+  const api = nock(`${TEST_ENV.url}/api/${ETHERPAD_API_VERSION}/`);
+
+  const endpointAndParams = Object.entries(replies).map(
+    ([endpoint, response]) =>
+      new Promise<[endpoint: string, params: URLSearchParams]>((resolve, reject) => {
+        api
+          .get(`/${endpoint}`)
+          .query(true)
+          .reply((uri, body) => {
+            const url = new URL(uri, TEST_ENV.url);
+            // check that API key is always sent
+            expect(url.searchParams.get('apikey')).toEqual(TEST_ENV.apiKey);
+            resolve([endpoint, url.searchParams]);
+            return response;
+          });
+      }),
+  );
+
+  return Promise.all(endpointAndParams).then(Object.fromEntries);
+}
+
 describe('Service API', () => {
   let instance: BuildAppType;
-  let api: nock.Scope;
 
   beforeAll(async () => {
     instance = await buildApp({ options: TEST_ENV });
-    api = nock(`http://localhost:9001/api/${ETHERPAD_API_VERSION}/`);
   });
 
   describe('create a pad', () => {
-    /**
-     * Helper to setup an emulator for the etherpad server
-     * @param replies Overrides for the emulated response from etherpad
-     */
-    function setUpApi(replies?: {
-      createGroupIfNotExists?: [
-        statusCode: number,
-        payload?: { code: number; message: string; data: { groupID: string } },
-      ];
-      createGroupPad?: [
-        StatusCodes: number,
-        payload?: { code: number; message: string; data: null },
-      ];
-    }) {
-      const groupID = replies?.createGroupIfNotExists?.[1]?.data ?? 'g.s8oes9dhwrvt0zif';
-
-      const groupMapper = new Promise((resolve, reject) => {
-        api
-          .get('/createGroupIfNotExistsFor')
-          .query(true)
-          .reply((uri, body) => {
-            // retrieve request params
-            const url = new URL(uri, TEST_ENV.url);
-            const parsed = url.searchParams;
-
-            // check that our server sent the right params
-            expect(parsed.get('apikey')).toEqual(TEST_ENV.apiKey);
-            expect(parsed.get('groupMapper')).toBeDefined();
-            resolve(parsed.get('groupMapper'));
-
-            // emulate etherpad reply
-            return (
-              replies?.createGroupIfNotExists ?? [
-                StatusCodes.OK,
-                {
-                  code: 0,
-                  message: 'ok',
-                  data: { groupID },
-                },
-              ]
-            );
-          });
-      });
-
-      const padName = new Promise((resolve, reject) => {
-        api
-          .get('/createGroupPad')
-          .query(true)
-          .reply((uri, body) => {
-            // retrieve request params
-            const url = new URL(uri, TEST_ENV.url);
-            const parsed = url.searchParams;
-
-            // check that our server sent the right params
-            expect(parsed.get('apikey')).toEqual(TEST_ENV.apiKey);
-            expect(parsed.get('groupID')).toEqual(groupID);
-            expect(parsed.get('padName')).toBeDefined();
-            resolve(parsed.get('padName'));
-
-            // emulate etherpad reply
-            return (
-              replies?.createGroupPad ?? [StatusCodes.OK, { code: 0, message: 'ok', data: null }]
-            );
-          });
-      });
-
-      const promisifiedGroupID = Promise.resolve(groupID);
-      return [groupMapper, padName, promisifiedGroupID];
-    }
-
     const payloadCreate = {
       method: HttpMethod.POST,
       url: '/etherpad/create',
@@ -98,24 +65,42 @@ describe('Service API', () => {
 
     it('creates a pad successfully', async () => {
       const { app } = instance;
-      const reqsParams = setUpApi();
+      const reqsParams = setUpApi({
+        createGroupIfNotExistsFor: [
+          StatusCodes.OK,
+          { code: 0, message: 'ok', data: { groupID: MOCK_GROUP_ID } },
+        ],
+        createGroupPad: [StatusCodes.OK, { code: 0, message: 'ok', data: null }],
+      });
       const res = await app.inject(payloadCreate);
 
-      // check that the groupMapper sent to etherpad is equal to the generated padID
-      const [groupMapper, padName, groupID] = await Promise.all(reqsParams);
-      expect(groupMapper).toEqual(padName);
+      const { createGroupIfNotExistsFor, createGroupPad } = await reqsParams;
+      expect(createGroupPad?.get('groupID')).toEqual(MOCK_GROUP_ID);
+      // groupMapper sent to etherpad is equal to the generated padID
+      expect(createGroupIfNotExistsFor?.get('groupMapper')).toEqual(createGroupPad?.get('padName'));
 
       expect(res.statusCode).toEqual(StatusCodes.OK);
       expect(res.json()).toEqual({
         ...MOCK_ITEM,
         name: 'test-item-name',
-        extra: { etherpad: { padID: `${groupID}$${padName}`, groupID } },
+        extra: {
+          etherpad: {
+            padID: `${MOCK_GROUP_ID}$${createGroupPad?.get('padName')}`,
+            groupID: MOCK_GROUP_ID,
+          },
+        },
       });
     });
 
     it('returns error on etherpad HTTP error', async () => {
       const { app } = instance;
-      setUpApi({ createGroupPad: [StatusCodes.GATEWAY_TIMEOUT] });
+      setUpApi({
+        createGroupIfNotExistsFor: [
+          StatusCodes.OK,
+          { code: 0, message: 'ok', data: { groupID: MOCK_GROUP_ID } },
+        ],
+        createGroupPad: [StatusCodes.GATEWAY_TIMEOUT],
+      });
       const res = await app.inject(payloadCreate);
       expect(res.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
       expect(res.json()).toMatchObject({
@@ -128,6 +113,10 @@ describe('Service API', () => {
     it('returns error on etherpad server error: pad does already exist', async () => {
       const { app } = instance;
       setUpApi({
+        createGroupIfNotExistsFor: [
+          StatusCodes.OK,
+          { code: 0, message: 'ok', data: { groupID: MOCK_GROUP_ID } },
+        ],
         createGroupPad: [
           StatusCodes.OK,
           { code: 1, message: 'pad does already exist', data: null },
@@ -145,6 +134,10 @@ describe('Service API', () => {
     it('returns error on etherpad server error: groupID does not exist', async () => {
       const { app } = instance;
       setUpApi({
+        createGroupIfNotExistsFor: [
+          StatusCodes.OK,
+          { code: 0, message: 'ok', data: { groupID: MOCK_GROUP_ID } },
+        ],
         createGroupPad: [
           StatusCodes.OK,
           { code: 1, message: 'groupID does not exist', data: null },
@@ -161,26 +154,13 @@ describe('Service API', () => {
 
     it('deletes pad on item creation error', async () => {
       const { app, spies } = instance;
-      const reqsParams = setUpApi();
-
-      // setup intercept for /deletePad call
-      const deletedPadId = new Promise((resolve, reject) => {
-        api
-          .get('/deletePad')
-          .query(true)
-          .reply((uri, body) => {
-            // retrieve request params
-            const url = new URL(uri, TEST_ENV.url);
-            const parsed = url.searchParams;
-
-            // check that our server sent the right params
-            expect(parsed.get('apikey')).toEqual(TEST_ENV.apiKey);
-            expect(parsed.get('padID')).toBeDefined();
-            resolve(parsed.get('padID'));
-
-            // emulate etherpad reply
-            return [StatusCodes.OK, { code: 0, message: 'ok', data: null }];
-          });
+      const reqsParams = setUpApi({
+        createGroupIfNotExistsFor: [
+          StatusCodes.OK,
+          { code: 0, message: 'ok', data: { groupID: MOCK_GROUP_ID } },
+        ],
+        createGroupPad: [StatusCodes.OK, { code: 0, message: 'ok', data: null }],
+        deletePad: [StatusCodes.OK, { code: 0, message: 'ok', data: null }],
       });
 
       // override item creation: ensure that the task fails
@@ -198,8 +178,10 @@ describe('Service API', () => {
         statusCode: 500,
       });
 
-      const [_, padName, groupID] = await Promise.all(reqsParams);
-      expect(await deletedPadId).toEqual(`${groupID}$${padName}`);
+      const { createGroupPad, deletePad } = await reqsParams;
+      expect(deletePad?.get('padID')).toEqual(`${MOCK_GROUP_ID}$${createGroupPad?.get('padName')}`);
     });
   });
+
+  describe('view a pad', () => {});
 });
