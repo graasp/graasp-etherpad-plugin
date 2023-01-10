8ee108cd31d27f91df3452ab3836c5a34400fd8f
Alexandre Chau <accounts@chau.moe>
2023-01-10 16:18:45 +0100

    feat: add cookie domain option

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,24 @@
 import { UnknownExtra } from '@graasp/sdk';
 
 export interface EtherpadPluginOptions {
-  /** URL (incl. protocol and port) of the etherpad server */
+  /**
+   * URL (incl. protocol and port) of the etherpad server
+   */
   url: string;
-  /** Public URL (incl. protocol and port) of the etherpad server, e.g.
-   *  if the back-end communicates with the etherpad service through a
-   *  private network (optional, will default to {@link url})
+  /**
+   * Public URL (incl. protocol and port) of the etherpad server, e.g.
+   * if the back-end communicates with the etherpad service through a
+   * private network (optional, will default to {@link url})
    */
   publicUrl?: string;
-  /** secret api key to authorize this app against the etherpad server */
+  /**
+   * Cookie domain to be used for the etherpad cookie (optional, will default
+   * to the hostname of {@link url})
+   */
+  cookieDomain?: string;
+  /**
+   * secret api key to authorize this app against the etherpad server
+   */
   apiKey: string;
 }
 
